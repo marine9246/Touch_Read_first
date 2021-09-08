4093eec68cb4b3cfe1319ebb54385d2986ba7036
marine9246 <marine9246@gmail.com>
2021-09-08 13:27:01 +0900

    Touch Read Demo first initial FW
    Vrs検出で励磁ON（励磁中はSPK有り）、トルク測定オプション追加　初期FWとしてコミット

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=AG_demo_Touch_Read.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/AG_demo_Touch_Read.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=agdemotouchread.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/agdemotouchread.x.tar
